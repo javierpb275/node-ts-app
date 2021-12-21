e6d3dc33e461bd86a63489906f436ae7586b202f
javierpb275 <javierpb275@gmail.com>
2021-12-21 18:52:05 +0100

    set express-handlebars configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import exphbs from "express-handlebars";
+import path from 'path';
 
 //Initializations
 const app = express();
 
 //Settings
 app.set("port", process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs.engine({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    helpers: require('./lib/helpers')
+}));
+
+app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(express.json()); //so I can understand the json object that get to my server.
