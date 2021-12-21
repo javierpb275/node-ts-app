bed22c84c9ba62a38890b1240b4def583f898c9f
javierpb275 <javierpb275@gmail.com>
2021-12-21 18:13:17 +0100

    formatted code and added middlewares to app.use: express.json() and express.urlencoded().

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import express from 'express';
+import express from "express";
 
 //Initializations
 const app = express();
 
 //Settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-//Middlewares 
+//Middlewares
+app.use(express.json()); //so I can understand the json object that get to my server.
+app.use(express.urlencoded({ extended: false })); //when an html form send me some data I can understand it
 
 //Routes
 
-//Static files 
+//Static files
 
 //starting the server
-app.listen(app.get('port'), () => {
-    console.log(`Server is up on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server is up on port ${app.get("port")}`);
 });
